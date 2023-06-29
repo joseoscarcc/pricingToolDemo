33ce92c04b2ac1a13cdd7a2000fae3100ab1b2af
MfJOE <joseoscarcc@gmail.com>
2023-06-28 22:58:31 -0600

    auth security includede

--- config.py
+++ config.py
@@ -7,4 +7,6 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     mapbox_access_token = os.environ.get('mapbox_access_token')
+    type_1 = os.environ.get('type_1')
+    type_2 = os.environ.get('type_2')
     
